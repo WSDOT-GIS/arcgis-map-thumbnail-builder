c99050eb91755e4428b13f308a0fa6b8c57c9436
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-10-01 10:47:12 -0700

    Added comments to map-to-canvas.js
    Added LICENSE and README.md to solution.

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 ArcGIS Map thumbnail builder
 ============================
 
-Exports an image of an ArcGIS API for JavaScript map.
+Exports an image of an [ArcGIS API for JavaScript] map.
+
+## Limitations ##
+
+* There does not currently seem to be a way for Internet Explorer users to save the image displayed in the canvas, whereas Chrome and Firefox users can either right-click on the canvas or click the link below the canvas to save the image.
+
+	This IE issue may be CORS related. It may be possible to work around it by using a proxy to call remote resources.
+
+## Modules ##
+
+### map-to-canvas ###
+
+Exports the contents of an [esri/Map] to an HTML [Canvas] element.
+
+Note that the [dojoConfig.gfxRenderer] option must be set to *"canvas"* in order for graphics layers to be exported.
+
+```javascript
+var dojoConfig = {
+	gfxRenderer: "canvas"
+}
+```
+
+
+### layerFactory ###
+
+Allows many types of map layers to be generated from a URL without knowing ahead of time what layer type will be needed.
+
+This module was copied from [its own repository](https://github.com/WSDOT-GIS/LayerFactory). 
+It was copied instead of using a [git submodule] so that the site would work as a [GitHub Project Page].
+
+[ArcGIS API for JavaScript]:https://developers.arcgis.com/javascript/
+[Canvas]:https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+[dojoConfig.gfxRenderer]:http://dojotoolkit.org/reference-guide/1.10/dojox/gfx.html#renderer-options
+[esri/Map]:https://developers.arcgis.com/javascript/jsapi/map-amd.html
+[GitHub Project Page]:https://help.github.com/articles/creating-project-pages-manually
+[git submodule]:http://www.git-scm.com/book/en/Git-Tools-Submodules
--- Script/map-to-canvas.js
+++ Script/map-to-canvas.js
@@ -20,30 +20,33 @@ define(["dojo/Deferred", "dojo/promise/all"], function (Deferred, all) {
 	 * Creates a canvas element that displays the contents of a map.
 	 * @param {esri/Map} map
 	 * @param {HTMLCanvasElement} canvas
+	 * @returns {dojo/promise/Promise}
 	 */
 	function mapToCanvas(map, canvas) {
-		var ctx;
+		// TODO: Add the ability to specify image generation parameters (e.g., DPI).
+
+		var ctx, requests;
 		ctx = canvas.getContext("2d");
 		// Clear any existing data from the canvas.
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+		// Set the canvas's size to match that of the map.
 		canvas.width = map.width;
 		canvas.height = map.height;
-		if (!ctx) {
-			ctx = canvas.getContext("2d");
-		}
-		var requests = [];
+		
+		// This array will contain Deferreds indicating when the images have loaded.
+		requests = [];
 
 		// Loop through map layers
 		map.layerIds.forEach(function (layerId) {
-			var layer = map.getLayer(layerId);
-			var url;
-			var exportParams;
-			var image;
-			var deferred;
+			var layer, url, exportParams, image, deferred;
+
+			layer = map.getLayer(layerId);
 
 			if (layer.url) {
+				// Create Deferred for current image loading.
 				deferred = new Deferred();
+				// Setup map service image export parameters.
 				exportParams = {
 					f: "image",
 					size: [map.width, map.height].join(","),
@@ -52,29 +55,39 @@ define(["dojo/Deferred", "dojo/promise/all"], function (Deferred, all) {
 					format: "png",
 					transparent: true
 				};
+				// Convert params to query string.
 				exportParams = objToQuery(exportParams);
+				// Create the export URL.
 				url = [layer.url, "/export?", exportParams].join("");
+				// Eliminate double slashes before "export".
 				url = url.replace("//export", "/export");
 
+				// Create the image element.
 				image = new Image(map.width, map.height);
 				image.crossOrigin = "anonymous";
+				// Add the current Deferred to the array.
 				requests.push(deferred);
+				// Add an event listener that will resolve the Deferred once the image has loaded.
 				image.addEventListener("load", function () {
-					// ctx.drawImage(image, 0, 0);
 					deferred.resolve(image);
 				}, false);
+				// Set the image's src attribute. This will begin the image loading.
 				image.src = url;
 			} else {
+				// If the layer doesn't have a URL property, log info to console.
 				console.log("No URL for layer: " + layerId);
 			}
 		});
 
 		// Once all of the images have loaded, add them to the canvas.
 		return all(requests).then(function (images) {
+			// Add the map server images to the canvas.
 			images.forEach(function (image) {
 				ctx.drawImage(image, 0, 0);
 			});
 
+			// Get all of the graphics layer canvases in the map's root element
+			// and add them to the combined map image canvas.
 			var canvases = map.root.querySelectorAll("canvas");
 			var tempCanvas;
 
@@ -82,6 +95,7 @@ define(["dojo/Deferred", "dojo/promise/all"], function (Deferred, all) {
 				tempCanvas = canvases[i];
 				ctx.drawImage(tempCanvas, 0, 0);
 			}
+			// Save the canvas image. (This allows the user to revert this version if further changes are made.)
 			ctx.save();
 		});
 	}
